3e99eea54797b99cb4eae3bb4ddf4755b2e23e47
Victor Longui Pereira <longuivictor@gmail.com>
2023-08-25 03:27:31 -0300

    adicionando dados em dicionário

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 frutas = []
+registros = []
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', )
 def principal():
     # frutas = ["Morango", "Uva", "Maçã", "Mamão", "Pera", "Melão", "Caju", "Mirtilo"]
     if request.method == "POST":
@@ -14,10 +15,13 @@ def principal():
     return render_template("index.html", frutas=frutas)
     # por padrão, é comum chamarmos com o mesmo nome função=idade(nome=nome)
 
-@app.route('/sobre')
+@app.route('/sobre', methods=["GET", "POST"])
 def sobre():
-    notas = {"Fulano": 5.0, "Beltrano": 6.0, "Aluno": 7.0, "Sicrano": 8.5, "Rodrigo": 9.5}
-    return render_template("sobre.html", notas=notas)
+    # notas = {"Fulano": 5.0, "Beltrano": 6.0, "Aluno": 7.0, "Sicrano": 8.5, "Rodrigo": 9.5}
+    if request.method == "POST":
+        if request.form.get("aluno") and request.form.get("nota"):
+            registros.append({"aluno": request.form.get("aluno"), "nota": request.form.get("nota")})
+    return render_template("sobre.html", registros=registros)
 
 if __name__ == "__main__":
     app.run(debug=True)
