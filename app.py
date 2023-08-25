4fc1c4f5cdf46485268222eecff5e78e51adf4fb
Victor Longui Pereira <longuivictor@gmail.com>
2023-08-25 02:54:20 -0300

    retornando valores de dicionario

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def principal():
-    frutas = ["Morango", "Uva", "Maçã", "Mamão"]
+    frutas = ["Morango", "Uva", "Maçã", "Mamão", "Pera", "Melão", "Caju", "Mirtilo"]
     return render_template("index.html", frutas=frutas)
     # por padrão, é comum chamarmos com o mesmo nome função=idade(nome=nome)
 
 @app.route('/sobre')
 def sobre():
-    return render_template("sobre.html")
+    notas = {"Fulano": 5.0, "Beltrano": 6.0, "Aluno": 7.0, "Sicrano": 8.5, "Rodrigo": 9.5}
+    return render_template("sobre.html", notas=notas)
 
 if __name__ == "__main__":
     app.run(debug=True)
